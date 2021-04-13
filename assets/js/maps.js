faf8a3b83587ba51be5a6263b9b26ed57b6fac88
Jonasitoelprogramador <jonahparrylloyd@gmail.com>
2021-04-13 11:40:49 +0000

    new js file with map initialization and marker

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,7 +1,7 @@
 let map;
 
 function initMap() {
-    var center = new google.maps.LatLng(37.422, -122.084058)
+    var center = new google.maps.LatLng(20.422, -100.084058)
     map = new google.maps.Map(document.getElementById("map"), {
         center: center,
         zoom: 8,
@@ -16,4 +16,20 @@ function initMap() {
     var service = new google.maps.PlacesService(map);
 
     service.nearbySearch(request, callback);
+}
+
+function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+        }
+    }
+}
+
+function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+    });
 }
