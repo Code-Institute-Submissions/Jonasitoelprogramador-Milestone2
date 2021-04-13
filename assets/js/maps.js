b633d3a14ebbb8dd0ae026e40ce228fca1763a8f
Jonasitoelprogramador <jonahparrylloyd@gmail.com>
2021-04-13 09:23:34 +0000

    creation of a placesService object

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,11 +1,19 @@
 let map;
 
 function initMap() {
+    var center = new google.maps.LatLng(37.422, -122.084058)
     map = new google.maps.Map(document.getElementById("map"), {
-        center: {
-            lat: -34.397,
-            lng: 150.644
-        },
+        center: center,
         zoom: 8,
     });
+
+    var request = {
+        location = center,
+        radius = 8047,
+        types = ['cafe']
+    };
+
+    var service = new google.maps.PlacesService(map);
+
+    service.nearbySearch(request, callback);
 }
